831d174a2a4c7c39b88bbf037cb011c3a8ce9785
alpay <calalli1903@gmail.com>
2023-08-05 15:52:13 +0400

    Project setup done.

---
+++ GitHubFollowers/FavoritesVC.swift
@@ -0,0 +1,17 @@
+//
+//  FavoritesVC.swift
+//  GitHubFollowers
+//
+//  Created by Alpay Calalli on 05.08.23.
+//
+
+import UIKit
+
+class FavoritesVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ GitHubFollowers/SearchVC.swift
@@ -0,0 +1,17 @@
+//
+//  SearchVC.swift
+//  GitHubFollowers
+//
+//  Created by Alpay Calalli on 05.08.23.
+//
+
+import UIKit
+
+class SearchVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+
+}
