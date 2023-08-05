0259d6c75f2e4ba5f17b3ca87a89444d5a9032ca
alpay <calalli1903@gmail.com>
2023-08-05 23:30:57 +0400

    SearchViewController UI done.

---
+++ GitHubFollowers/Components/GFButton.swift
@@ -0,0 +1,35 @@
+//
+//  GFButton.swift
+//  GitHubFollowers
+//
+//  Created by Alpay Calalli on 05.08.23.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .blue
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+    }
+}
